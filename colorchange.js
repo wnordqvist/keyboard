052da0ed612e850778b2d8ad06949cf4466a681f
Wiktoria Nordqvist <46751885+wnordqvist@users.noreply.github.com>
2019-06-10 10:53:15 +0200

    Update colorchange.js

--- colorchange.js
+++ colorchange.js
@@ -1,9 +1,11 @@
+
+//Function for when a key is pressed.
 document.addEventListener('keydown', function (event) {
     console.log(event.code);
 
     document.getElementById(event.code).classList.add("pressed-button");
 });
-// Tar bort samma class när knappen släpps
+// Function for when key is released
 document.addEventListener('keyup', function(event) {
     console.log(event.code);
     
